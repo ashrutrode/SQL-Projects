9d7fdc3fd6fecd94e5b6de75f19d4a17027266e2
ashrutrode <143514581+ashrutrode@users.noreply.github.com>
2023-09-20 19:56:59 -0400

    Update tsla-stock-performance.py

--- tsla-stock-performance.py
+++ tsla-stock-performance.py
@@ -1,3 +1,6 @@
+# get user_response
+user_input = request.get_json()
+
 # get the start and end dates
 startDate = user_input[0]
 endDate = user_input[1]
@@ -5,13 +8,6 @@ endDate = user_input[1]
 # convert to int then back to str
 startDate = str(int(startDate))
 endDate = str(int(endDate))
-
-# login credentials for mySQL database
-# insert your own credentials here
-dbhost = "XXXXXX";
-dbuser = "XXXXXX";
-dbpass = "XXXXXX";
-dbname = "XXXXXX";
 
 # create connection
 conn = mysql.connector.connect(
@@ -22,6 +18,73 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
+# insert data into database
+def data_insertion():
+
+	# extracting data from csv
+	import csv
+	file = open("static/data/tsla.csv", "r")
+	tsla_data = list(csv.reader(file, delimiter=","))
+	file.close()
+
+	# convert all the data - skip 0th element since its the column name
+	for i in range(1, len(tsla_data)):
+
+		# convert the first element (the date) to a numerical format of YYYYMMDD
+		date = tsla_data[i][0]
+		year = "20"+date[6:8]
+		month = date[:2]
+		day = date[3:5]
+		dateNum = int(year+month+day)
+		tsla_data[i][0] = dateNum
+
+		# convert the open, high, low, close data to floats
+		for j in range(1, 5):
+		    tsla_data[i][j] = float(tsla_data[i][j])
+
+		# delete last element, the volume
+		tsla_data[i] = tsla_data[i][:-1]
+
+	# display the result
+	print("Step #1: The count of the array is " + str(len(tsla_data)) + ".");
+
+	# sql to create the table
+	sql = '''CREATE TABLE tsla_stock (
+		date INT,
+		open FLOAT,
+		high FLOAT,
+		low FLOAT,
+		close FLOAT
+	)'''
+	#cursor.execute(sql)
+	#result = cursor.fetchall()
+
+	# display the result
+	print("Step #2: The table tsla_stock was successfully created.");
+	
+	# inserting data into table
+	row_counter = 0
+	for i in range(1, len(tsla_data)):
+
+		# get all information
+		date = tsla_data[i][0];
+		open_ = tsla_data[i][1];
+		high = tsla_data[i][2];
+		low = tsla_data[i][3];
+		close = tsla_data[i][4];
+
+		# peform the query
+		sql = f"INSERT INTO tsla_stock (date, open, high, low, close) VALUES ({date}, {open_}, {high}, {low}, {close})";
+		cursor.execute(sql)
+		result = cursor.fetchall()
+
+		# iterate the counter if successful
+		if result:
+			row_counter = row_counter + 1
+
+	# show the results
+	print("Step #3: " + str(row_counter) + " rows were inserted.")
+
 # result function
 def show_result(result):
 	for x in result: 
@@ -31,40 +94,44 @@ def show_result(result):
 def get_data():
 
 	# all stock prices
-	sql = f'''SELECT * 
+	sql = '''SELECT * 
 		FROM tsla_stock 
-		WHERE date >= {startDate} AND date <= {endDate}'''
-	cursor.execute(sql)
+		WHERE date >= %s AND date <= %s'''
+	sql_tuple = (startDate, endDate)
+	cursor.execute(sql, sql_tuple)
 	result = cursor.fetchall()
 	#show_result(result)
 
 	# stock price: highest
-	sql = f'''SELECT close 
+	sql = '''SELECT close 
 			FROM tsla_stock 
-			WHERE date >= {startDate} AND date <= {endDate}
+			WHERE date >= %s AND date <= %s
 			ORDER BY close DESC
 			LIMIT 1'''
-	cursor.execute(sql)
+	sql_tuple = (startDate, endDate)
+	cursor.execute(sql, sql_tuple)
 	result = cursor.fetchall()
 	stock_price_highest = result[0][0]
 	#show_result(result)
 
 	# stock price: lowest
-	sql = f'''SELECT low 
+	sql = '''SELECT low 
 			FROM tsla_stock 
-			WHERE date >= {startDate} AND date <= {endDate}
+			WHERE date >= %s AND date <= %s
 			ORDER BY close ASC
 			LIMIT 1'''
-	cursor.execute(sql)
+	sql_tuple = (startDate, endDate)
+	cursor.execute(sql, sql_tuple)
 	result = cursor.fetchall()
 	stock_price_lowest = result[0][0]
 	#show_result(result)
 
 	# stock price: average
-	sql = f'''SELECT AVG(close) 
+	sql = '''SELECT AVG(close) 
 			FROM tsla_stock 
-			WHERE date >= {startDate} AND date <= {endDate}'''
-	cursor.execute(sql)
+			WHERE date >= %s AND date <= %s'''
+	sql_tuple = (startDate, endDate)
+	cursor.execute(sql, sql_tuple)
 	result = cursor.fetchall()
 	stock_price_average = result[0][0]
 	#show_result(result)
@@ -72,10 +139,11 @@ def get_data():
 	# stock price: median
 
 	# first, get the number of rows in the dates
-	sql = f'''SELECT COUNT(*) 
+	sql = '''SELECT COUNT(*) 
 			FROM tsla_stock 
-			WHERE date >= {startDate} AND date <= {endDate}'''
-	cursor.execute(sql)
+			WHERE date >= %s AND date <= %s'''
+	sql_tuple = (startDate, endDate)
+	cursor.execute(sql, sql_tuple)
 	result = cursor.fetchall()
 	count_rows = result[0][0];
 	#show_result(result)
@@ -91,14 +159,15 @@ def get_data():
 		median_row_minus1 = int(median_row-1);
 
 		# get the nth row, plus next element since we need average of the two medians
-		sql = f'''SELECT AVG(close)
+		sql = '''SELECT AVG(close)
 					FROM (
 						SELECT close 
 						FROM tsla_stock 
-						WHERE date >= {startDate} AND date <= {endDate}
-						LIMIT {median_row_minus1}, 2
-					) q''' 
-		cursor.execute(sql)
+						WHERE date >= %s AND date <= %s
+						LIMIT %s, 2
+					) q'''
+		sql_tuple = (startDate, endDate, median_row_minus1)
+		cursor.execute(sql, sql_tuple)
 		result = cursor.fetchall()
 		stock_price_median = result[0][0]
 		#show_result(result)
@@ -111,20 +180,22 @@ def get_data():
 		median_row_minus1 = int(median_row-1);
 
 		# to get the nth row (rows start at 1, not 0), we do LIMIT n-1, 1
-		sql = f'''SELECT close 
+		sql = '''SELECT close 
 			FROM tsla_stock 
-			WHERE date >= {startDate} AND date <= {endDate}
-			LIMIT {median_row_minus1}, 1'''
-		cursor.execute(sql)
+			WHERE date >= %s AND date <= %s
+			LIMIT %s, 1'''
+		sql_tuple = (startDate, endDate, median_row_minus1)
+		cursor.execute(sql, sql_tuple)
 		result = cursor.fetchall()
 		stock_price_median = result[0][0]
 		#show_result(result)
 
 	# standard deviation
-	sql = f'''SELECT STDDEV(close) 
+	sql = '''SELECT STDDEV(close) 
 			FROM tsla_stock
-			WHERE date >= {startDate} AND date <= {endDate}'''
-	cursor.execute(sql)
+			WHERE date >= %s AND date <= %s'''
+	sql_tuple = (startDate, endDate)
+	cursor.execute(sql, sql_tuple)
 	result = cursor.fetchall()
 	stock_price_stddev = result[0][0]
 	#show_result(result)
@@ -133,7 +204,8 @@ def get_data():
 	return [stock_price_highest, stock_price_lowest, stock_price_average, stock_price_median, stock_price_stddev]
 
 # call function
+#data_insertion()
 data_to_return = get_data()
 
-# print the result
-print(data_to_return)
+# return the result
+return jsonify(data_to_return)
