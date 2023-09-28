73d5243d1dd4d3fe81da6d35a72dbd156b03bedc
ashrutrode <143514581+ashrutrode@users.noreply.github.com>
2023-09-28 01:24:01 -0400

    Update tsla-stock-performance.py

--- tsla-stock-performance.py
+++ tsla-stock-performance.py
@@ -92,72 +92,41 @@ def show_result(result):
 
 # get data
 def get_data():
-
+	
 	# all stock prices
 	sql = '''SELECT * 
 		FROM tsla_stock 
 		WHERE date >= %s AND date <= %s'''
 	sql_tuple = (startDate, endDate)
-	cursor.execute(sql, sql_tuple)
-	result = cursor.fetchall()
+	#cursor.execute(sql, sql_tuple)
+	#result = cursor.fetchall()
 	#show_result(result)
-
-	# stock price: highest
-	sql = '''SELECT close 
+	
+	# get the highest, lowest, average, std dev, and number of prices
+	sql = '''SELECT MAX(high), MIN(low), AVG(close), STDDEV(close), COUNT(*)   
 			FROM tsla_stock 
-			WHERE date >= %s AND date <= %s
-			ORDER BY close DESC
-			LIMIT 1'''
+			WHERE date >= %s AND date <= %s'''
 	sql_tuple = (startDate, endDate)
 	cursor.execute(sql, sql_tuple)
 	result = cursor.fetchall()
+	
+	# save those vars accordingly
 	stock_price_highest = result[0][0]
-	#show_result(result)
-
-	# stock price: lowest
-	sql = '''SELECT low 
-			FROM tsla_stock 
-			WHERE date >= %s AND date <= %s
-			ORDER BY close ASC
-			LIMIT 1'''
-	sql_tuple = (startDate, endDate)
-	cursor.execute(sql, sql_tuple)
-	result = cursor.fetchall()
-	stock_price_lowest = result[0][0]
-	#show_result(result)
-
-	# stock price: average
-	sql = '''SELECT AVG(close) 
-			FROM tsla_stock 
-			WHERE date >= %s AND date <= %s'''
-	sql_tuple = (startDate, endDate)
-	cursor.execute(sql, sql_tuple)
-	result = cursor.fetchall()
-	stock_price_average = result[0][0]
-	#show_result(result)
-
-	# stock price: median
-
-	# first, get the number of rows in the dates
-	sql = '''SELECT COUNT(*) 
-			FROM tsla_stock 
-			WHERE date >= %s AND date <= %s'''
-	sql_tuple = (startDate, endDate)
-	cursor.execute(sql, sql_tuple)
-	result = cursor.fetchall()
-	count_rows = result[0][0];
-	#show_result(result)
-
-	# second, find the average based on whether there are
+	stock_price_lowest = result[0][1]
+	stock_price_average = result[0][2]
+	stock_price_stddev = result[0][3]
+	count_rows = result[0][4]
+	
+	# second, find the median based on whether there are
 	# an even or odd number of rows returned
-
+	
 	# count even
 	if count_rows%2 == 0:
-
+	
 		# get the first median by dividing by 2 
-		median_row = count_rows/2;
-		median_row_minus1 = int(median_row-1);
-
+		median_row = count_rows/2
+		median_row_minus1 = int(median_row-1)
+	
 		# get the nth row, plus next element since we need average of the two medians
 		sql = '''SELECT AVG(close)
 					FROM (
@@ -171,14 +140,14 @@ def get_data():
 		result = cursor.fetchall()
 		stock_price_median = result[0][0]
 		#show_result(result)
-
+	
 	# count odd
 	elif count_rows%2 == 1:
 	    
 		# mysql rows start at 1, so +1 and divide by 2 to get median row
-		median_row = (count_rows+1)/2;
-		median_row_minus1 = int(median_row-1);
-
+		median_row = (count_rows+1)/2
+		median_row_minus1 = int(median_row-1)
+	
 		# to get the nth row (rows start at 1, not 0), we do LIMIT n-1, 1
 		sql = '''SELECT close 
 			FROM tsla_stock 
@@ -189,17 +158,7 @@ def get_data():
 		result = cursor.fetchall()
 		stock_price_median = result[0][0]
 		#show_result(result)
-
-	# standard deviation
-	sql = '''SELECT STDDEV(close) 
-			FROM tsla_stock
-			WHERE date >= %s AND date <= %s'''
-	sql_tuple = (startDate, endDate)
-	cursor.execute(sql, sql_tuple)
-	result = cursor.fetchall()
-	stock_price_stddev = result[0][0]
-	#show_result(result)
-
+	
 	# the result
 	return [stock_price_highest, stock_price_lowest, stock_price_average, stock_price_median, stock_price_stddev]
 
