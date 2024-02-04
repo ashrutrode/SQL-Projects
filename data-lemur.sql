d2630a8f6c113ed2d5da8a6790740b0d14cef557
Ashrut Rode <143514581+ashrutrode@users.noreply.github.com>
2024-02-03 23:43:30 -0500

    Update data-lemur.sql

--- data-lemur.sql
+++ data-lemur.sql
@@ -9,7 +9,6 @@ HAVING count(*) >= 3;
 
 
 
-
 --Page With No Likes [Facebook SQL Interview Question]
 with pages_with_likes as (
   SELECT page_id, count(*)
@@ -22,7 +21,6 @@ left join pages_with_likes
 on pages.page_id=pages_with_likes.page_id
 where pages_with_likes.page_id is NULL
 ORDER BY pages.page_id;
-
 
 
 
@@ -49,3 +47,69 @@ select
     from viewership
     where device_type in ('tablet', 'phone')
   ) as mobile_views
+
+
+
+
+
+--Average Post Hiatus (Part 1) [Facebook SQL Interview Question]
+with cte as (
+  SELECT user_id, count(*) AS count_posts
+  FROM posts
+  WHERE EXTRACT(YEAR FROM post_date) = 2021
+  GROUP BY user_id
+  HAVING count(*) >= 2
+)
+
+select 
+  posts.user_id, 
+  DATE_PART('days', MAX(posts.post_date)-MIN(posts.post_date)) AS days_between
+from posts
+JOIN cte ON cte.user_id = posts.user_id
+group by posts.user_id;
+
+
+
+
+
+--Teams Power Users [Microsoft SQL Interview Question]
+SELECT sender_id, count(*) as message_count 
+FROM messages
+GROUP BY sender_id
+ORDER BY count(*) DESC
+LIMIT 2;
+
+
+
+
+
+--Duplicate Job Listings [Linkedin SQL Interview Question]
+with dupl_cte as 
+(
+  SELECT company_id, count(*)
+  FROM job_listings
+  GROUP BY company_id
+  HAVING count(*) >= 2
+)
+
+SELECT count(*) as duplicate_companies
+FROM dupl_cte;
+
+
+
+
+
+--Cities With Completed Trades [Robinhood SQL Interview Question]
+SELECT city, count(*) 
+FROM trades
+JOIN users ON trades.user_id = users.user_id
+WHERE status='Completed'
+GROUP BY city
+ORDER BY count(*) DESC
+LIMIT 3;
+
+
+
+
+
+
